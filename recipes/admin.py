1516e4d737f6da38ff70d01dc5efdffd42d16c23
AnnikaW-dev <warmannika@gmail.com>
2025-05-04 15:41:31 +0200

    Create the Recipe app & model

---
+++ recipes/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Recipe
+
+# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'meal_type',
+        'calories',
+        'instruction',
+        'ingredients',
+        'images'
+    )
+    list_filter = ('meal_type',)
